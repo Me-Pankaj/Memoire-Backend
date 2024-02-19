ffbf9cfdf0d51def29039a40d58023ebce2d48a2
Pankaj Gupta <guptapankaj6363@gmail.com>
2024-02-19 23:12:40 +0530

    First commit

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  updateUserProfile,
+  userLogin,
+  userSignUp,
+} from "../controllers/userController.js";
+import { userAuth } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/test2", (req, res) => {
+  console.log("Hii");
+  res.send("Helo from Route");
+});
+
+
+router.post("/register", userSignUp);
+router.post("/login", userLogin);
+router.put("/profile", userAuth, updateUserProfile);
+
+export default router;
